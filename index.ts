6f8c11538a39b1d3151cda91c74427dc0ab2d935
eugene teu <eugeneteu@gmail.com>
2020-08-31 01:28:29 +0800

    get ready for deployment

--- index.ts
+++ index.ts
@@ -3,11 +3,25 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import { testRouter, quotesRouter } from './routes';
 import cors from 'cors';
+import mongo from 'mongodb';
+import path from 'path';
+/*
+const MongoClient = mongo.MongoClient;
+const uri = process.env.dbClientString;
+if (!uri) {
+    throw new Error('err');
+}
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+    const collection = client.db('test').collection('devices');
+    // perform actions on the collection object
+    client.close();
+});*/
 
 const app = express();
 // Setup server port
 const PORT = process.env.PORT || 8080;
-
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(
     bodyParser.urlencoded({
         extended: true,
@@ -21,7 +35,9 @@ app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 
 app.use('/test', testRouter);
 app.use('/quotes', quotesRouter);
-
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 mongoose
     .connect('mongodb://localhost/cs3219-task-b', {
         useNewUrlParser: true,
@@ -29,6 +45,7 @@ mongoose
     })
     .then((_) => {
         const db = mongoose.connection;
+        mongoose.set('useFindAndModify', false);
         if (!db) {
             console.log('Error connecting db');
             app.listen(PORT, () => {
@@ -45,3 +62,16 @@ mongoose
             });
         }
     });
+
+/*
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://<username>:<password>@cluster0.e2rp0.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+    */
