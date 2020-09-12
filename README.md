4452262e016e492fbab2cb553674b19dd0ca27a7
eugene teu <eugeneteu@gmail.com>
2020-09-12 22:25:11 +0800

    add readme and dotenv

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ Deployed on Google App Engine
 Tested with Mocha and chai
 
 CI/CD with Travis.
+
+## Getting Started
+
+1. Visit [Deployment on GAE](https://cs3219-rest-api-eugene.et.r.appspot.com/)
+
+## Getting Started (Local Development)
+
+1. clone the repo
+1. cd into root
+1. Create a `.env` file and set `NODE_ENV='development'` in the file.
+1. run `npm run-script dev`
+1. Go to [local host 8080](http://localhost:8080/)
+1. Frontend is served and you should be connected directly to the database in mongodb atlas.
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -8,7 +9,7 @@ import path from 'path';
 const app = express();
 // Setup server port
 const PORT = process.env.PORT || 8080;
-app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.use(
     bodyParser.urlencoded({
         extended: true,
@@ -19,16 +20,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/test', testRouter);
 app.use('/quotes', quotesRouter);
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.use(express.static(path.join(__dirname, './client/build')));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, './client/build/index.html'));
+    });
+} else {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+}
 
 //'mongodb://localhost/cs3219-task-b'
 //'mongodb+srv://user1:user1@cluster0.e2rp0.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority'
 mongoose
     .connect(
-        'mongodb+srv://user1:user1@cluster0.e2rp0.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority' ??
-            '',
+        'mongodb+srv://user1:user1@cluster0.e2rp0.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority',
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
